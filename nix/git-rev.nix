8fbe7db9186751dc837730c472d17e7b16163e7f
Nick <nick@typeable.io>
2021-05-11 15:04:35 +0000

    Add post text.

--- nix/git-rev.nix
+++
@@ -1,8 +0,0 @@
-with import ./stackyaml.nix {};
-
-let
-  gitRev = path:
-    builtins.readFile (pkgs.runCommand "git" { preferLocalBuild = true; } ''
-      ${pkgs.git}/bin/git -C ${builtins.toString path} rev-parse --verify HEAD > $out
-    '').out;
-in { inherit gitRev; }
